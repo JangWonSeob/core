d913ca1a6c7e6490032f9f421b5f733e5ff4c81b
jang13579 <80467890+jang13579@users.noreply.github.com>
2022-03-07 00:16:19 +0900

    주문, 할인 정책 관련 비즈니스 로직 구현(순수 자바)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,18 +1,30 @@
 package hello.core;
 
+import hello.core.discount.DisCountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
-
 
     public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    public DisCountPolicy disCountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(),disCountPolicy());
     }
 
 }
